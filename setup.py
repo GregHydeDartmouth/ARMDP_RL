973921c76c465a1f215cc8b110ab6ed6941e417e
GregHyde <gregory.m.hyde.th@dartmouth.edu>
2022-12-19 20:00:12 -0500

    removed evals dir and added a setup.py file to build

---
+++ setup.py
@@ -0,0 +1,27 @@
+import os
+import sys
+import re
+import io
+
+from setuptools import find_packages
+from setuptools import setup
+
+__version__ = '1.0'
+
+REQUIRED_PACKAGES = [
+        'gurobipy',
+        'tqdm',
+        'graphviz',
+        'gym'
+]
+
+setup(
+    name='abstraction_machines',
+    version=__version__,
+    author='Greg Hyde',
+    author_email='gregory.m.hyde.th@dartmouth.edu',
+    description='abstraction machine',
+    packages=find_packages(),
+    install_requires=REQUIRED_PACKAGES,
+    python_requires='>=3.8',
+)
