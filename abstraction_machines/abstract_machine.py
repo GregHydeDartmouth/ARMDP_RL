060e3761deed111630f750e99fcc3cfdec5bc6a0
GregHyde <gregory.m.hyde.th@dartmouth.edu>
2022-12-22 20:34:38 -0500

    updated to solve Q function and allow action selection/stepping/ and adding trajectories that incrementally add evidence.

--- abstraction_machines/abstract_machine.py
+++ abstraction_machines/abstract_machine.py
@@ -1,41 +1,56 @@
-import os
+import random
 from tqdm import tqdm
 from gurobipy import *
 from graphviz import Digraph
 from collections import defaultdict
 
 class AbstractionMachine():
-    def __init__(self, trajectories, verbose=False):
+    def __init__(self, trajectories = [], verbose=False, run_q_vals=False, action_set=None, gamma=0.9):
         self.verbose = verbose
-        self.action_mapping, self.state_mapping, self.exemplar_trajectories = \
-            self._make_mappings(trajectories)
+        self.run_q_vals = run_q_vals
+        if self.run_q_vals:
+            self.abstract_table = defaultdict(lambda: defaultdict(lambda: defaultdict(set)))
+            if action_set == None:
+                raise Exception("can not run q_vals without action_size")
+            else:
+                self.action_set = action_set
+        self.gamma = gamma
+        #self.action_mapping = dict()
+        #self.state_mapping = dict()
+        #self.state_num = 0
+        #self.action_num = 0
+        self.default_triple_set = set()
+        self.exemplar_trajectories = trajectories
+        self.current_state = None
+        self.depth = 2
 
+    '''
     def _make_mappings(self, trajectories):
-        action_mapping = dict()
-        state_mapping = dict()
         mapped_trajectories = []
-        state_num = 0
-        action_num = 0
         for traj in trajectories:
             mapped_trajectory = []
             for i, triple in enumerate(traj):
                 state, action, reward, next_state = triple
                 if i == 0:
-                    if str(state) not in state_mapping:
-                        state_mapping[str(state)] = str(state_num)
-                        state_num += 1
-                if str(action) not in action_mapping:
-                    action_mapping[str(action)] = str(action_num)
-                    action_num += 1
-                if str(next_state) not in state_mapping:
-                    state_mapping[str(next_state)] = str(state_num)
-                    state_num += 1
-                new_state = state_mapping[str(state)]
-                new_action = action_mapping[str(action)]
-                new_next_state = state_mapping[str(next_state)]
+                    if str(state) not in self.state_mapping:
+                        self.state_mapping[str(state)] = str(self.state_num)
+                        self.state_mapping[str(self.state_num)] = str(state)
+                        self.state_num += 1
+                if str(action) not in self.action_mapping:
+                    self.action_mapping[str(action)] = str(self.action_num)
+                    self.action_mapping[str(self.action_num)] = str(action)
+                    self.action_num += 1
+                if str(next_state) not in self.state_mapping:
+                    self.state_mapping[str(next_state)] = str(self.state_num)
+                    self.state_mapping[str(self.state_num)] = str(next_state)
+                    self.state_num += 1
+                new_state = self.state_mapping[str(state)]
+                new_action = self.action_mapping[str(action)]
+                new_next_state = self.state_mapping[str(next_state)]
                 mapped_trajectory.append([new_state, new_action, reward,new_next_state])
             mapped_trajectories.append(mapped_trajectory)
-        return action_mapping, state_mapping, mapped_trajectories
+        return mapped_trajectories
+    '''
 
     def _write_mappings(self):
         actions_file = open('action_mapping.txt', 'w')
@@ -108,7 +123,6 @@ class AbstractionMachine():
                 drp.addConstr(quicksum(outcome_toggles) <= 1)
                 constraints += 1
 
-
         # ensure mutual exclusivity over conflicting reward triples
         for triple, reward_dict in reward_mutex_dict.items():
             for reward, choice_reward_dict_list in reward_dict.items():
@@ -119,7 +133,7 @@ class AbstractionMachine():
                                 for choice, choice_var in choice_reward_dict.items():
                                     drp.addConstr(choice_var + other_choice_reward_dict[choice], GRB.LESS_EQUAL, 1, name='reward_mutex_constr')
 
-                                    
+
         for choice_type, choices in choice_types.items():
             choice_toggle = drp.addVar(name='{}_jump_toggle'.format(choice_type), vtype=GRB.BINARY)
             drp.addGenConstrIndicator(choice_toggle, False, quicksum(choices), GRB.EQUAL, 0)
@@ -130,50 +144,162 @@ class AbstractionMachine():
 
         return reward_table, jump_contexts
 
-    def resolve_reward_conflicts(self, depth=2, write_file=False, make_graph=False):
+    def _solve_abstract_MDP(self, tf=None):
+        self.abstract_Q_table = defaultdict(lambda: defaultdict(float))
+        for state in self.abstract_Q_table:
+            for action in self.action_set:
+                self.abstract_Q_table[state][action] = 0
+        print('solving abstract MDP')
+        while True:
+            delta = 0
+            for state, action_dict in self.abstract_table.items():
+                for action, next_state_dict in action_dict.items():
+                    qsa = 0
+                    for next_state, rewards in next_state_dict.items():
+                        reward_vals = list(rewards)
+                        assert len(reward_vals) == 1, 'something wrong with rewards'
+                        if tf is not None:
+                            probability = tf[state.split('^')[0]][action][next_state.split('^')[0]]
+                        else:
+                            probability = 1
+                        next_state_qsa, next_state_qsa_action = self._abstract_qsa_max(next_state)
+                        qsa += probability * reward_vals[0] + self.gamma * next_state_qsa
+                    old_qsa = self.abstract_Q_table[state][action]
+                    delta += abs(old_qsa - qsa)
+                    self.abstract_Q_table[state][action] = qsa
+            if self.verbose:
+                print('Delta: {}'.format(delta))
+            if delta <= 1e-2:
+                break
+
+    def _abstract_qsa_max(self, state):
+        max_qsa = None
+        max_action = None
+        equivalent_actions = []
+        for action in self.action_set:
+            val = round(self.abstract_Q_table[state][action], 5)
+            if max_qsa is None:
+                max_qsa = val
+                max_action = action
+                equivalent_actions = [(max_qsa, max_action)]
+            else:
+                if val > max_qsa:
+                    max_qsa = val
+                    max_action = action
+                    equivalent_actions = [(max_qsa, max_action)]
+                elif val == max_qsa:
+                    equivalent_actions.append((val, action))
+        if len(equivalent_actions) > 1:
+            max_qsa, max_action = random.choice(equivalent_actions)
+        return max_qsa, max_action
+
+    def get_action(self, state, eps = 0.1):
+        if random.random() < eps:
+            action = random.choice(self.action_set)
+            return action
+        else:
+            if self.current_state is None:
+                self.current_state = '{}^[{}]'.format(state, 0)
+            max_qsa, max_action = self._abstract_qsa_max(self.current_state)
+            return max_action
+
+    def add_trajectory(self, trajectory, write_file = False, make_graph = False):
+        resolve = False
+        add_traj = False
+        hold_current_state = self.current_state
+        self.current_state = '{}^[{}]'.format(trajectory[0][0], 0)
+        for triple in trajectory:
+            if str(triple) not in self.default_triple_set:
+                self.default_triple_set.add(str(triple))
+                add_traj = True
+            state, action, reward, next_state = triple
+            conflict = self.step(state, action, reward, next_state)
+            if conflict:
+                resolve = True
+                add_traj = True
+                break
+        if add_traj:
+            self.exemplar_trajectories.append(trajectory)
+        if resolve:
+            self.resolve_reward_conflicts(write_file=write_file, make_graph=make_graph)
+        self.current_state = hold_current_state
+
+    def step(self, state, action, reward, next_state):
+        conflict = False
+        if self.current_state is None:
+            self.current_state = '{}^[{}]'.format(state, 0)
+        if action in self.abstract_table[self.current_state]:
+            found_abstract_state = None
+            for some_next_state in self.abstract_table[self.current_state][action]:
+                if some_next_state.split('^')[0] == next_state:
+                    found_abstract_state = some_next_state
+                    break
+            if found_abstract_state is None:
+                current_level = self.current_state.split('^')[1]
+                next_state = '{}^{}'.format(next_state, current_level)
+                self.abstract_table[self.current_state][action][next_state].add(reward)
+                self.current_state = next_state
+            else:
+                self.abstract_table[self.current_state][action][some_next_state].add(reward)
+                if len(self.abstract_table[self.current_state][action][some_next_state]) > 1:
+                    conflict = True
+                self.current_state = some_next_state
+        else:
+            current_level = self.current_state.split('^')[1]
+            next_state = '{}^{}'.format(next_state, current_level)
+            self.abstract_table[self.current_state][action][next_state].add(reward)
+            self.current_state = next_state
+        return conflict
+
+    def reset(self):
+        self.current_state = None
+
+    def resolve_reward_conflicts(self, write_file=False, make_graph=False):
         print("\nMaking Dual Reward Problem")
 
         while True:
             drp = Model("Dual Reward Problem")
             # get split states
-            reward_table, jump_contexts = self._get_transition_splits(drp, depth)
+            reward_table, jump_contexts = self._get_transition_splits(drp, self.depth)
             z = drp.addVar(name='objective', vtype=GRB.INTEGER)
             drp.addConstr(z == quicksum(jump_contexts), name='objective_constr')
             drp.setObjective(z, GRB.MINIMIZE)
             if write_file:
-                drp.write('model_with_depth={}.lp'.format(depth))
-                self._write_mappings()
+                drp.write('model_with_depth={}.lp'.format(self.depth))
             drp.optimize()
             var_dict = dict()
             try:
                 for v in drp.getVars():
                     var_dict[v.varName] = v.x
             except:
-                depth += 1
-                print("Depth insufficient to solve... increasing depth by 1. New Depth: {}".format(depth))
+                self.depth += 1
+                print("Depth insufficient to solve... increasing depth by 1. New Depth: {}".format(self.depth))
                 continue
-            abstract_table = self.build_abstract_MDP(var_dict, reward_table)
+            self.build_abstract_MDP(var_dict, reward_table)
+            if self.run_q_vals:
+                self._solve_abstract_MDP()
             if make_graph:
-                self.build_hierarchical_MDP_graph(var_dict, reward_table)
+                self.build_abstract_MDP_graph(var_dict, reward_table)
             break
-        return abstract_table
+        return
 
     def build_abstract_MDP(self, var_dict, reward_table):
-        abstract_table = defaultdict(lambda: defaultdict(lambda: defaultdict(set)))
+        self.abstract_table = defaultdict(lambda: defaultdict(lambda: defaultdict(set)))
         for variable, value in var_dict.items():
             if '_choice' in variable and value > 0.5:
                 reward = reward_table[variable]
                 triple = variable.split('(')[1].split(')')[0].split(',')
-                abstract_table[triple[0]][triple[1]][triple[2]].add(reward)
-        return abstract_table
+                self.abstract_table[triple[0]][triple[1]][triple[2]].add(reward)
 
-    def build_hierarchical_MDP_graph(self, var_dict, reward_table):
-        g = Digraph('hierarchical_MDP')
+    def build_abstract_MDP_graph(self, var_dict, reward_table):
+        g = Digraph('abstract_MDP')
         edges = set()
         for variable, value in var_dict.items():
             if '_choice' in variable and value > 0.5:
                 reward = reward_table[variable]
                 triple = variable.split('(')[1].split(')')[0].split(',')
+                if self.run_q_vals:
+                    q_sa = round(self.abstract_Q_table[triple[0]][triple[1]],2)
                 if tuple(triple) not in edges:
                     try:
                         scale = 4
@@ -189,6 +315,9 @@ class AbstractionMachine():
                     except:
                         g.node(triple[0])
                         g.node(triple[2])
-                    g.edge(triple[0], triple[2], label='a={},r={}'.format(triple[1], reward))
+                    if self.run_q_vals:
+                        g.edge(triple[0], triple[2], label='a={},r={},q_sa={}'.format(triple[1], reward, q_sa))
+                    else:
+                        g.edge(triple[0], triple[2], label='a={},r={}'.format(triple[1], reward))
                     edges.add(tuple(triple))
-        g.render('graphs/hierarchical_MDP', view=False)
+        g.render('graphs/abstract_MDP', view=False)
--- unittests/tests.py
+++ unittests/tests.py
@@ -51,7 +51,7 @@ class test_abstract_q_learning(unittest.TestCase):
         AM = AbstractionMachine(trajectories)
         AM.resolve_reward_conflicts()
 
-    def test_error(self):
+    def est_error(self):
         actions = {'^': '^',
                    'v': 'v',
                    '<': '<',
@@ -93,3 +93,58 @@ class test_abstract_q_learning(unittest.TestCase):
         AM = AbstractionMachine(trajectories)
         AM.resolve_reward_conflicts(write_file=True, make_graph=True)
 
+    def est_add_trajectories(self):
+        actions = {'^': '^',
+                   'v': 'v',
+                   '<': '<',
+                   '>': '>',
+                   'o': 'o'}
+        t1 = [['1', actions['^'], 0, '4'],
+              ['4', actions['>'], 0, '5'],
+              ['5', actions['^'], 0, '8'],
+              ['8', actions['v'], 0, '5'],
+              ['5', actions['>'], 0, '6'],
+              ['6', actions['^'], 2, '9']]
+        t2 = [['1', actions['^'], 0, '4'],
+              ['4', actions['>'], 0, '5'],
+              ['5', actions['>'], 0, '6'],
+              ['6', actions['o'], 0, '6'],
+              ['6', actions['^'], 1, '9']]
+        AM = AbstractionMachine([t1,t2], run_q_vals=True, action_set=['^', 'v', '<', '>', 'o'], verbose=True)
+        AM.resolve_reward_conflicts(make_graph=True)
+        t3 = [['1', actions['v'], 0, '5'],
+              ['5', actions['>'], 0, '6'],
+              ['6', actions['o'], 0, '6'],
+              ['6', actions['^'], 0.5, '9']]
+        AM.add_trajectory(t3, make_graph=True)
+
+    def test_one_traj_at_a_time(self):
+        actions = {'^': '^',
+                   'v': 'v',
+                   '<': '<',
+                   '>': '>',
+                   'o': 'o'}
+        t1 = [['1', actions['^'], 0, '4'],
+              ['4', actions['>'], 0, '5'],
+              ['5', actions['^'], 0, '8'],
+              ['8', actions['v'], 0, '5'],
+              ['5', actions['>'], 0, '6'],
+              ['6', actions['^'], 2, '9']]
+        AM = AbstractionMachine(run_q_vals=True, action_set=['^', 'v', '<', '>', 'o'], verbose=True)
+        AM.add_trajectory(t1)
+        t2 = [['1', actions['^'], 0, '4'],
+              ['4', actions['>'], 0, '5'],
+              ['5', actions['>'], 0, '6'],
+              ['6', actions['o'], 0, '6'],
+              ['6', actions['^'], 1, '9']]
+        AM.add_trajectory(t2)
+        t3 = [['1', actions['v'], 0, '5'],
+              ['5', actions['>'], 0, '6'],
+              ['6', actions['o'], 0, '6'],
+              ['6', actions['^'], 0.5, '9']]
+        AM.add_trajectory(t3, make_graph=True)
+        AM.reset()
+        action = AM.get_action('1', eps=0)
+        AM.step('1',action, 0, '5')
+        action = AM.get_action('5')
+        print(action)
