5b9b57b68cc032f7b2b517004a3eeb964d7931d6
GregHyde <gregory.m.hyde.th@dartmouth.edu>
2023-07-19 17:37:44 -0400

    set abstraction_machine to old formulation

--- abstraction_machines/abstraction_machine.py
+++ abstraction_machines/abstraction_machine.py
@@ -4,27 +4,36 @@ from collections import defaultdict
 import matplotlib.pyplot as plt
 
 class AbstractionMachine():
-    def __init__(self, conflicting_trajectories, granularity='state'):
+    def __init__(self, conflicting_trajectories, granularity='state', monotonic_levels = False):
         self.conflicting_trajectories = conflicting_trajectories
         self.granularity = granularity
+        self.monotonic_levels = monotonic_levels
         self.solution_set = None
 
     def make_cross_product_graph(self):
         if self.solution_set is not None:
             g = graphviz.Digraph('AMDP', format='png')
+
             for k, trajectory in enumerate(self.conflicting_trajectories):
                 for l, triple in enumerate(trajectory):
                     state, action, reward, next_state = triple
+                    # trajectories always start at base level (i.e., 0 here)
+                    toggle_depth = self.depth
                     if l == 0:
-                        split_state = '{}^{}'.format(state, 0)
-                    for i in range(self.depth):
-                        if self.solution_set['traj_{}_triple_{}_next_state_{}_level_{}'.format(k, l, next_state, i)] == 1:
-                            split_next_state = '{}^{}'.format(next_state, i)
-                    g.node(split_state, shape='box')
-                    g.node(split_next_state, shape='box')
-                    g.edge(split_state, split_next_state, label='a={}/r={}'.format(action, reward))
-                    split_state = split_next_state
+                        toggle_depth = 1
+                    for i in range(0, toggle_depth):
+                        # imposing monotonicity on levels (i.e., can only move up in levels, not down)
+                        j_start = 0
+                        if self.monotonic_levels:
+                            j_start = i
+                        for j in range(j_start, self.depth):
+                            if self.solution_set['traj_{}_triple_{}_({}^{},{},{}^{})'.format(k, l, state, i, action, next_state, j)] == 1:
+                                g.node('{}^{}'.format(state, i), shape='box')
+                                g.node('{}^{}'.format(next_state, j), shape='box')
+                                g.edge('{}^{}'.format(state, i), '{}^{}'.format(next_state, j), label='a={}/r={}'.format(action, reward))
+                                break
             g.render(filename="graphs/AMDP", format="png")
+            
 
     def get_triggers(self):
         if self.solution_set is not None:
@@ -47,13 +56,13 @@ class AbstractionMachine():
         while True:
             conflict_resolver = Model("Conflict Resolver")
 
-            jump_types, reward_jump_types = self._sum_to_one_constraint(conflict_resolver, depth)
-            self._reward_mutex_constraint(reward_jump_types, conflict_resolver)
-            change_level_indicators = self._transition_ambiguity_constraint(jump_types, conflict_resolver)
+            transition_ambiguity_dict, reward_ambiguity_dict = self._sum_to_one_constraint(conflict_resolver, depth)
+            self._reward_ambiguity_constraint(reward_ambiguity_dict, conflict_resolver)
+            level_change_indicators = self._transition_ambiguity_constraint(transition_ambiguity_dict, conflict_resolver)
 
             # OBJECTIVE
             z = conflict_resolver.addVar(name='objective', vtype=GRB.INTEGER)
-            conflict_resolver.addConstr(z == quicksum(change_level_indicators), name='objective_constr')
+            conflict_resolver.addConstr(z == quicksum(level_change_indicators), name='objective_constr')
             conflict_resolver.addConstr(z >= min_obj, name='objective_floor_constr')
             conflict_resolver.setObjective(z, GRB.MINIMIZE)
             conflict_resolver.write('test.lp')
@@ -61,6 +70,7 @@ class AbstractionMachine():
             if conflict_resolver.status == GRB.OPTIMAL:
                 min_obj = conflict_resolver.objVal
                 self.solution_set = dict()
+                self.depth = depth
                 for v in conflict_resolver.getVars():
                     self.solution_set[v.varName] = v.x
                 break
@@ -70,81 +80,92 @@ class AbstractionMachine():
 
     def _sum_to_one_constraint(self, conflict_resolver, depth):
 
-        jump_types = defaultdict(lambda: defaultdict(lambda: defaultdict(list)))
-        reward_jump_types = defaultdict(lambda: defaultdict(lambda: defaultdict(lambda: defaultdict(list))))
-
+        transition_ambiguity_dict = defaultdict(lambda: defaultdict(lambda: defaultdict(list)))
+        reward_ambiguity_dict = defaultdict(lambda: defaultdict(lambda: defaultdict(lambda: defaultdict(list))))
         for k, trajectory in enumerate(self.conflicting_trajectories):
-            state_toggles = None
+            prev_toggles_by_next_state_level = defaultdict(list)
             for l, triple in enumerate(trajectory):
                 state, action, reward, next_state = triple
-                # ENTRY LEVEL CONSTR
-                if state_toggles is None:
-                    state_toggle = conflict_resolver.addVar(vtype=GRB.BINARY, name='traj_{}_triple_{}_state_{}_level_{}'.format(k, l, state, 0))
-                    conflict_resolver.addConstr(state_toggle == 1, 'entry_level_constr')
-                    state_toggles = [state_toggle]
 
-                next_state_toggles = []
-                for j in range(depth):
-                    next_state_toggle = conflict_resolver.addVar(vtype=GRB.BINARY, name='traj_{}_triple_{}_next_state_{}_level_{}'.format(k, l, next_state, j))
-                    next_state_toggles.append(next_state_toggle)
+                # trajectories always start at base level (i.e., 0 here)
+                toggle_depth = depth
+                if l == 0:
+                    toggle_depth = 1
 
-                for i, state_toggle in enumerate(state_toggles):
-                    for j, next_state_toggle in enumerate(next_state_toggles):
+                split_triples = []
+                toggles_by_state_level = defaultdict(list)
+                toggles_by_next_state_level = defaultdict(list)
+                for i in range(0, toggle_depth):
+
+                    # imposing monotonicity on levels (i.e., can only move up in levels, not down)
+                    j_start = 0
+                    if self.monotonic_levels:
+                        j_start = i
+
+                    for j in range(j_start, depth):
+                        triple_toggle = conflict_resolver.addVar(name='traj_{}_triple_{}_({}^{},{},{}^{})'.format(k, l, state, i, action, next_state, j), vtype=GRB.BINARY)
+                        # for continuity
+                        toggles_by_state_level[i].append(triple_toggle)
+                        toggles_by_next_state_level[j].append(triple_toggle)
+                        # for sum to 1
+                        split_triples.append(triple_toggle)
                         if self.granularity == 'state':
-                            jump_types['{}'.format(next_state)][i][j].append((state_toggle, next_state_toggle))
-                            reward_jump_types['{}'.format(next_state)][i][j][reward].append((state_toggle, next_state_toggle))
+                            # for transition ambiguity
+                            transition_ambiguity_dict['{}'.format(next_state)][i][j].append(triple_toggle)
+                            # for reward ambiguity
+                            reward_ambiguity_dict['{}'.format(next_state)][i][j][reward].append(triple_toggle)
                         elif self.granularity == 'triple':
-                            jump_types['{},{},{}'.format(state, action, next_state)][i][j].append((state_toggle, next_state_toggle))
-                            reward_jump_types['{},{},{}'.format(state, action, next_state)][i][j][reward].append((state_toggle, next_state_toggle))
-                # SUM TO ONE CONSTRAINT
-                conflict_resolver.addConstr(quicksum(next_state_toggles) == 1, name='sum_to_one_constr')
-                state_toggles = next_state_toggles
-        return jump_types, reward_jump_types
+                            # for transition ambiguity
+                            transition_ambiguity_dict['{},{},{}'.format(state, action, next_state)][i][j].append(triple_toggle)
+                            # for reward ambiguity
+                            reward_ambiguity_dict['{},{},{}'.format(state, action, next_state)][i][j][reward].append(triple_toggle)
+                        
+                        
+                ## TRAJECTORY CONTINUITY CONSTR
+                for prev_next_state_level, next_state_level_toggles in prev_toggles_by_next_state_level.items():
+                    state_level_toggles = toggles_by_state_level[prev_next_state_level]
+                    conflict_resolver.addConstr(quicksum(next_state_level_toggles) == quicksum(state_level_toggles))
+                prev_toggles_by_next_state_level = toggles_by_next_state_level
+
+                ## SUM TO ONE CONSTR
+                conflict_resolver.addConstr(quicksum(split_triples) == 1)
+
+        return transition_ambiguity_dict, reward_ambiguity_dict
     
-    def _reward_mutex_constraint(self, reward_jump_types, conflict_resolver):
-        # REWARD MUTEX CONSTRAINT
-        for jump_type, i_dict in reward_jump_types.items():
+    def _reward_ambiguity_constraint(self, reward_ambiguity_dict, conflict_resolver):
+
+        for toggle_type, i_dict in reward_ambiguity_dict.items():
             for i, j_dict in i_dict.items():
                 for j, reward_dict in j_dict.items():
                     if len(reward_dict.keys()) == 1:
                         continue
-                    reward_toggles_types = []
-                    for reward, toggle_type_pairs in reward_dict.items():
-                        toggle_type = []
-                        for toggle_type_pair in toggle_type_pairs:
-                            toggle = conflict_resolver.addVar(vtype=GRB.BINARY)
-                            conflict_resolver.addConstr(quicksum(toggle_type_pair) >= 1.6 - 100000 * (1-toggle))
-                            conflict_resolver.addConstr(quicksum(toggle_type_pair) <= 1.4 + 100000 * (toggle))
-                            toggle_type.append(toggle)
-                        toggle_type_sum = conflict_resolver.addVar(vtype=GRB.BINARY)
-                        conflict_resolver.addConstr(quicksum(toggle_type) >= 0.6 - 100000 * (1 - toggle_type_sum))
-                        conflict_resolver.addConstr(quicksum(toggle_type) <= 0.4 + 100000 * (toggle_type_sum))
-                        reward_toggles_types.append(toggle_type_sum)
-                    conflict_resolver.addConstr(quicksum(reward_toggles_types) <= 1)
-
-    def _transition_ambiguity_constraint(self, jump_types, conflict_resolver):
-        # TRANSITION AMBIGUITY CONSTRAINT
-        # also get level change indicators for objective
-        change_level_indicators = []
-        for jump_type, i_dict in jump_types.items():
+                    toggle_type_indicators = []
+                    for reward, toggles in reward_dict.items():
+                        toggle_type_indicator = conflict_resolver.addVar(name='{},{},{},{}'.format(toggle_type, i, j, reward), vtype=GRB.BINARY)
+                        # toggle type indicator is 1 if any of the toggles are in use, i.e., quicksum(toggles) > 1
+                        conflict_resolver.addConstr(quicksum(toggles) >= 0.6 - 100000 * (1 - toggle_type_indicator))
+                        conflict_resolver.addConstr(quicksum(toggles) <= 0.4 + 100000 * (toggle_type_indicator))
+                        toggle_type_indicators.append(toggle_type_indicator)
+                    ## REWARD AMBIGUITY CONSTR
+                    conflict_resolver.addConstr(quicksum(toggle_type_indicators) <= 1)
+                    
+    def _transition_ambiguity_constraint(self, transition_ambiguity_dict, conflict_resolver):
+        level_change_indicators = []
+        for toggle_type, i_dict in transition_ambiguity_dict.items():
             for i, j_dict in i_dict.items():
-                transition_ambiguity_sum = []
-                for j, toggle_type_pairs in j_dict.items():
-                    toggle_type = []
-                    for toggle_type_pair in toggle_type_pairs:
-                        toggle = conflict_resolver.addVar(vtype=GRB.BINARY)
-                        conflict_resolver.addConstr(quicksum(toggle_type_pair) >= 1.6 - 100000 * (1-toggle))
-                        conflict_resolver.addConstr(quicksum(toggle_type_pair) <= 1.4 + 100000 * (toggle))
-                        toggle_type.append(toggle)
-                    toggle_sum = conflict_resolver.addVar(vtype=GRB.BINARY)
-                    conflict_resolver.addConstr(quicksum(toggle_type) >= 0.6 - 100000 * (1 - toggle_sum))
-                    conflict_resolver.addConstr(quicksum(toggle_type) <= 0.4 + 100000 * (toggle_sum))
+                toggle_type_indicators = []
+                for j, toggles in j_dict.items():
+                    toggle_type_indicator = conflict_resolver.addVar(name='{},{},{}'.format(toggle_type, i, j), vtype=GRB.BINARY)
+                    # toggle type indicator is 1 if any of the toggles are in use, i.e., quicksum(toggles) > 1
+                    conflict_resolver.addConstr(quicksum(toggles) >= 0.6 - 100000 * (1 - toggle_type_indicator))
+                    conflict_resolver.addConstr(quicksum(toggles) <= 0.4 + 100000 * (toggle_type_indicator))
+                    toggle_type_indicators.append(toggle_type_indicator)
                     if i != j:
-                        change_level_indicators.append(toggle_sum)
-                    transition_ambiguity_sum.append(toggle_sum)
-                conflict_resolver.addConstr(quicksum(transition_ambiguity_sum) <= 1)
-        return change_level_indicators
-
+                        # for objective
+                        level_change_indicators.append(toggle_type_indicator)
+                ## TRANSITION AMBIGUITY CONSTR
+                conflict_resolver.addConstr(quicksum(toggle_type_indicators) <= 1)
+        return level_change_indicators
 
 if __name__ == "__main__":
     actions = {'^' : '0',
@@ -167,6 +188,6 @@ if __name__ == "__main__":
             ['11', actions['^'], 0, '15'],
             ['15', actions['>'], 2, '16']]
     trajectories.append(t)
-    AM = AbstractMachine(trajectories, granularity='state')
-    AM.solve()
+    AM = AbstractionMachine(trajectories, granularity='triple')
+    depth, min_obj = AM.solve()
     AM.make_cross_product_graph()
