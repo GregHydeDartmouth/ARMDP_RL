641f427ce89dc1973311621c1f7125344155a71e
GregHydeDarmouth <Gregory.M.Hyde.TH@dartmouth.edu>
2023-08-01 15:35:13 -0400

    make models for each rm in the officeworld task

--- toy_problems/evaluate_office_world.py
+++ toy_problems/evaluate_office_world.py
@@ -5,8 +5,8 @@ from office_world import OfficeWorld
 from abstraction_machines.abstract_agent import AbstractAgent
 
 actions = [a.value for a in Actions]
-ow = OfficeWorld(rf_id=1)
-aa = AbstractAgent(actions, granularity='state', monotonic_levels=True, learning_rate=0.1, discount_factor=0.95, exploration_rate=0.2)
+ow = OfficeWorld(rf_id=4)
+aa = AbstractAgent(actions, granularity='state', monotonic_levels=True, learning_rate=0.1, discount_factor=0.95, exploration_rate=0.1)
 rewards = deque(maxlen=100)
 max_steps = 200
 
@@ -27,4 +27,4 @@ for i in range(0, 100000):
     print("\rEpisode: {} Average reward: {}".format(i, reward_avg), end='')
 aa.graph_AMDP()
 aa.graph_RM()
-
+aa.save_triggers('rm_4')
